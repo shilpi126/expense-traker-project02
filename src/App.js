47ab241fc30d7bd79926cdb2094631e453ce95cb
shilpi <shilpi8624@gmail.com>
2024-07-17 15:16:17 +0530

    :airplane: verify emil id using firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import Signup from "./Pages/Signup/Signup"
 import Login from "./Pages/Login/Login";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Profile from "./Pages/Profile/Profile";
+import AuthContext from "./store/auth-context";
+import User from "./Pages/User/User";
 
 function App() {
+  const authCtx = useContext(AuthContext)
+
+
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Profile/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-      
+   
+      <Route path="/" element={authCtx.isLoggedIn ?    <User/>: <Navigate to="/login"/> }/> 
+      <Route path="/updateprofile" element={authCtx.isLoggedIn ? <Profile/>: <Navigate to="/login"/> }/> 
+      {authCtx.isLoggedIn && <Route path="/login" element={<Login/>}/>}
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </div>
   );
